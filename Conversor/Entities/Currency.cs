d9fe30bc91a296f3703e1907c6dd89a45dd1ab47
Luiz Henrique Tarda <luizftarda@gmail.com>
2018-08-20 21:02:17 -0300

    init

---
+++ Conversor/Entities/Currency.cs
@@ -0,0 +1,28 @@
+﻿using Conversor.Api.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conversor.Api.Domain.Entities
+{
+    public class Currency
+    {
+        private CurrencyIdentifier _identifier;
+        public CurrencyIdentifier Identifier
+        {
+            get => _identifier;
+            set
+            {
+                _identifier = value ?? throw new ArgumentNullException("Currency indentifier cannot be null");
+            }
+        }
+
+
+        public double DollarValue { get; set; }
+
+        public double? ConverterToOtherCurrency(Currency other, double? amount)
+        {
+            return null;
+        }
+    }
+}
---
+++ Conversor/ValueObjects/CurrencyIdentifier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conversor.Api.Domain.ValueObjects
+{
+    public class CurrencyIdentifier
+    {
+        private string _code;
+        public string Code
+        {
+            get => _code;
+            set
+            {
+                if (string.IsNullOrEmpty(value) &&
+                    string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("code cannot be num or Empty");
+                _code = value;
+            }
+        }
+
+        private string _country;
+        public string Country
+        {
+            get => _country;
+            set
+            {
+                if (string.IsNullOrEmpty(value) &&
+                   string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("country cannot be num or Empty");
+                _country = value;
+            }
+        }
+    }
+}
